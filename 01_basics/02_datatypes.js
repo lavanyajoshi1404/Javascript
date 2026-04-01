bafd6ba88fc60a81d4d2daeb7d5295cceac78d93
lavanyajoshi1404 <lavanyajoshi1404@gmail.com>
2026-04-01 13:32:21 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all js code as newer js version
+// alert(3+3) // we are using node js not browser
+//code redability should be high
+//Data Types
+let name ="Lavanya" //string
+let age =18        //number
+let isLoggedIn =true //boolean
+/*Data Types
+*Primitive*
+ number => 2 to power 53
+ bigint
+ string =>""
+ boolean =>true/false
+ null => standaolne value
+ undefined =>
+ symbol=>For unique
+ */
+//object 
+console.log(typeof null);//=>object
+console.log(typeof undefined)//=>undefined
