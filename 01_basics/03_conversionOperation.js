1c0446ef9dd12ac316e3bb8cfc8b1bab137973d9
lavanyajoshi1404 <lavanyajoshi1404@gmail.com>
2026-04-01 14:08:35 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+//let score ="abc" => NaN
+
+//let score=null => 0
+//let score =undefined =>NaN
+let score =true 
+// IF STRING THEN NAN
+console.log(typeof score)
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+//"33"=>33
+//"33abc"=>nan
+//true/falsee=> 0/1
+let isLoggedIn =""
+
+let booleanIs =Boolean(isLoggedIn)
+console.log(booleanIs)
+//1=>true ;0=>false
+//""=>false
+//"hitesh" =>true
+
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber) 
