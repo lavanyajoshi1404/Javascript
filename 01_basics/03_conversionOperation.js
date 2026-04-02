896a0d56ef7631149aef4f1cdc572c3c6cc5c279
lavanyajoshi1404 <lavanyajoshi1404@gmail.com>
2026-04-02 15:55:49 +0000

    conversion operations done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -2,13 +2,13 @@
 
 //let score=null => 0
 //let score =undefined =>NaN
-let score =true 
+/*let score =true 
 // IF STRING THEN NAN
-console.log(typeof score)
+console.log(typeof score)*/
 
-let valueInNumber=Number(score)
+/*let valueInNumber=Number(score)
 console.log(typeof valueInNumber)
-console.log(valueInNumber)
+console.log(valueInNumber)*/
 //"33"=>33
 //"33abc"=>nan
 //true/falsee=> 0/1
@@ -24,3 +24,27 @@ let someNumber=33
 let stringNumber=String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber) 
+
+let value=3
+let negValue=-value
+//console.log(negValue)
+
+console.log(2+2);//add
+console.log(2-2);//subtract
+console.log(2*2);//multiply
+console.log(2**3);//exponential
+console.log(2/3);//divide
+console.log(2%2);//modulas
+let str1="hello"
+let str2 ="Lavanya"
+
+let str3=str1+str2
+console.log(str3)
+console.log("1"+2+2)//=>122
+console.log(1+"2")//=>12
+console.log(1+2+"2")//=>132*/ 
+//tricky coversion
+//console.log(+true) =>1 and true+ not allowed
+//console.log(+"") =>1
+let num1,num2,num3
+num1=num2=num3=2+2 //dont write this type of code
