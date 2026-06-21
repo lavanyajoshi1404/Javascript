0f4de95da444ae46189d03337207c4ae46fa169b
lavanyajoshi1404 <lavanyajoshi1404@gmail.com>
2026-06-21 08:37:08 +0000

    math.pi function

--- 10_classesandobjects/mathpi.js
+++ 10_classesandobjects/mathpi.js
@@ -1,2 +1,22 @@
 const piDescriptor = Object.getOwnPropertyDescriptor(Math, 'PI');
 console.log(piDescriptor);
+
+
+const User={
+    name:'Lavanya',
+    age:20,
+    isAvailable:true
+}
+console.log(Object.getOwnPropertyDescriptor(User,'name'));
+
+Object.defineProperty(User,'name',{
+    writable:false,
+    configurable:false,
+    enumerable:true
+})
+console.log(Object.getOwnPropertyDescriptor(User,'name'));
+
+
+for(let [key,value] of Object.entries(User)){
+    console.log(`${key}: ${value}`);
+}
